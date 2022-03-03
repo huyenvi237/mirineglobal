63ff22824188740bf1e5c51cfaef437381632e88
Huyen Vi <100335881+huyenvi237@users.noreply.github.com>
2022-03-03 11:59:22 +0900

    update commit
    エラーを対応します。

--- mirineglobal/main1.py
+++ mirineglobal/main1.py
@@ -5,24 +5,18 @@ import plotly.graph_objects as go
 import pandas as pd
 import logging
 
-
 #データを取得
-
 def get_data(stock_name,time):
     # Datetimeを設定
-
-    start_date = datetime(2022, 1, 1)
     end_date = datetime.now()
 
     #Yahoo Financeからデータを取得
-    try:
-        dt=yf.download(stock_name,start=time,end=end_date)
-        dt.to_csv('stock_data1.csv')         #データをCSVファイルに書き込む
-        logging.info('CSVファイルを作成しました。')
-    except:
-        logging.error('エラーが発生しました')
+    dt=yf.download(stock_name,start=time,end=end_date)
+    dt.to_csv('stock_data1.csv')         #データをCSVファイルに書き込む
 
     dr = pd.read_csv('stock_data1.csv')
+
+    #Graphを設計
     fig = go.Figure(data=[go.Candlestick(x=dr['Date'],
                                          open=dr['Open'], high=dr['High'],
                                          low=dr['Low'], close=dr['Close'])
@@ -41,19 +35,6 @@ def get_data(stock_name,time):
     fig.show()
 
 if __name__ == "__main__":
-    #loggingが設定
-
-    logger = logging.getLogger('Output_file')
-    file_logger = logging.FileHandler('ouput_file.log')
-    new_format = '[%(asctime)s] - [%(levelname)s] - %(message)s'
-    file_logger_format = logging.Formatter(new_format)
-
-    file_logger.setFormatter(file_logger_format)
-    logger.addHandler(file_logger)
-    logger.setLevel(logging.INFO)
-
-    console = logging.StreamHandler()
-    console.setLevel(logging.INFO)
 
     args=sys.argv
     get_data(args[1],args[2])
