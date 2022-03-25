07a227ac0651abae75026bf321acb5ed0d6bafbb
Huyen Vi <100335881+huyenvi237@users.noreply.github.com>
2022-03-25 17:47:06 +0900

    flask file

---
+++ mirineglobal/table.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+# importing pandas module
+import pandas as pd
+
+app = Flask(__name__)
+
+# reading the data in the csv file
+df = pd.read_csv('stock.csv')
+df.to_csv('stock.csv', index=None)
+
+
+# route to html page - "table"
+@app.route('/')
+@app.route('/table')
+def table():
+    # converting csv to html
+    data = pd.read_csv('stock.csv')
+    return render_template('table.html', tables=[data.to_html()], titles=[''])
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=int("5000"))
