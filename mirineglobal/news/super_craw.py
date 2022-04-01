b43ab2fde3fb835622f78ed2c0f7333a49ff0b1d
Huyen Vi <100335881+huyenvi237@users.noreply.github.com>
2022-04-01 15:59:17 +0900

    コードの調整

--- mirineglobal/news/super_craw.py
+++ mirineglobal/news/super_craw.py
@@ -3,40 +3,31 @@ from bs4 import BeautifulSoup as bs
 from random import randint
 from time import sleep
 import csv
-import pandas as pd
 
-#クローリングURL
+### 必ずsleep timeをセットする。ページからblock可能性が高いです。
+### random ライブラリを使用すると時間が調整しやすい。
 URL = 'https://news.yahoo.co.jp/topics/top-picks?page='
 
-#必須なリスト作る
-titles=[]
-links=[]
-tit=[]
-li=[]
+#使用リスク
+titles=[]   #title list
+links=[]    #link list
+index=[]    #削除したelementsのインデックスを保存リスク
+content=[]  #content list
 
-#Page1-3をクローリングする
-for page in range(1, 3):
+for page in range(1, 2):
     req = requests.get(URL + str(page))
     soup = bs(req.text, 'html.parser')
 
-    tit = soup.find_all('div', {'class': 'newsFeed_item_title'})
-    li = soup.find_all('a', {'class': 'newsFeed_item_link'})
+    tit=soup.find_all('div', {'class': 'newsFeed_item_title'})
+    li=soup.find_all('a', {'class': 'newsFeed_item_link'})
 
+    for l in li:
+        links.append(l.attrs["href"])
+    for t in tit:
+        titles.append(t.text)
     sleep(randint(4, 10))
 
-#Titles, Linksのリストを作る
-for l in li:
-    links.append(l.attrs["href"])
-for t in tit:
-    titles.append(t.text)
-
-#CSVファイルを作成
-with open("new2.csv", "w", encoding='UTF-8') as f:
-    writer = csv.writer(f)
-    writer.writerow(["Titles","Links"])
-    writer.writerows(zip(titles, links))
-    
-#Contentを取得関数
+#ページによって関数を使用するかどうか決める
 def getMain_page(li):
     list_link=[]
     for links in li:
@@ -50,12 +41,30 @@ def getMain_page(li):
 
 craw_source=getMain_page(links)
 #print(craw_source)
+
+#アクセスできるリングを調整する
 for c in craw_source:
     if "articles" not in c:
-        craw_source.remove(c)
+        if len(index) == 0:
+            index.append(craw_source.index(c))
+            craw_source.remove(c)
+        else:
+            index.append((craw_source.index(c)+len(index)))
+            craw_source.remove(c)
 
-content=[]
+#リスクの保存ができるかどうか確認する
+print(len(index))      
+print(len(titles))
 
+#Titles listを調整する
+for i in sorted(index, reverse=True):
+    del  titles[i]
+
+#リスクの保存ができるかどうか確認する
+print(len(titles))
+print(len(craw_source))
+
+#Contentsを取得する
 for link in craw_source:
     main_news = requests.get(link)
     soup2 = bs(main_news.content, "html.parser")
@@ -63,8 +72,10 @@ for link in craw_source:
     y = body[0]
     content.append(y.findChild("p", {"class": "sc-giadOv loZBCE yjSlinkDirectlink highLightSearchTarget"}).text)
     sleep(randint(4, 10))
-    
-#CSVファイルにcontentを追加
-dt = pd.read_csv('new2.csv')
-dt['Contents'] = pd.Series(content)
-dt.to_csv('file.csv')
+
+#CSVファイルを作成
+with open("file.csv", "w", encoding='UTF-8') as f:
+    writer = csv.writer(f)
+    writer.writerow(["Titles","Links","Contents"])
+    writer.writerows(zip(titles, links, content))
+
